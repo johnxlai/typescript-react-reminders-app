3f4406de77ff84a795ae16ac731d7d9ab608d0a9
John Lai <lai.john.py@gmail.com>
2023-10-17 17:32:34 -0400

    Add remove reminder

--- src/services/reminder.ts
+++ src/services/reminder.ts
@@ -15,4 +15,11 @@ class ReminderService {
     const response = await this.http.post<Reminder>('/todos', {title})
     return response.data;
   }
+
+  async removeReminder(id:number) {
+    const response = await this.http.delete(`/todos/${id}`);
+    return response.data;
+  }
 }
+
+export default new ReminderService();
